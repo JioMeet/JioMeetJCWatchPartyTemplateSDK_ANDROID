f993bc6e0f526ef4e2061d0bcec9ccd5bf357e9d
sahil1.salgotra <sahil1.salgotra@ril.com>
2023-11-09 14:54:26 +0530

    Refactor permissions

--- SAMPLE_APP/app/src/main/java/com/example/sample_app/PermissionActivity.kt
+++ SAMPLE_APP/app/src/main/java/com/example/sample_app/PermissionActivity.kt
@@ -1,40 +1,55 @@
 package com.example.sample_app
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import org.jio.sdk.utils.extensions.showToast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 class PermissionActivity : ComponentActivity() {
+
+    private val conferencePermissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.BLUETOOTH_CONNECT
+    )
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val isNotGrantedAll = permissions.any { !it.value }
+            if (isNotGrantedAll) {
+                Toast.makeText(this, "Provide necessary permissions to proceed", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                launchMainActivity()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (HelperClass.checkPermissionForCorporateUsers(this) && HelperClass.isInternetAvailable(
-                this
-            )
-        ) {
-            setContent {
-                val joinCallIntent = Intent(this, MainActivity::class.java)
-                startActivity(joinCallIntent)
-            }
+
+        val isAllPermissionsGranted = conferencePermissions.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (!isAllPermissionsGranted) {
+            requestPermissionLauncher.launch(conferencePermissions)
         } else {
-            HelperClass.requestPermissionForCorporateUsers(this)
+            launchMainActivity()
         }
     }
 
+    private fun launchMainActivity() {
+        val joinCallIntent = Intent(this, MainActivity::class.java)
+        startActivity(joinCallIntent)
+        finish()
+    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-            val joinCallIntent = Intent(this, MainActivity::class.java)
-            startActivity(joinCallIntent)
-        } else {
-            showToast("Provide necessary permissions to proceed.")
-        }
+    override fun onBackPressed() {
+        finish()
     }
 }
